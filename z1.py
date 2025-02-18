fd39e1e3225addfb48a552a2b7015812fb91651f
Pedro Henrique Freitas da Silva <151584153+1freitasp@users.noreply.github.com>
2025-02-17 21:38:23 -0300

    Create z1.py

---
+++ z1.py
@@ -0,0 +1,16 @@
+## primeiro projeto usando laços de repetiçao
+## consiste em um jogo onde o usuario tem que inserir informaçoes iniciais e acertar uma pergunta
+
+nome = str(input("olá,qual o seu nome?"))
+print("olá", nome)
+jogo = str(input("quer jogar um jogo?"))
+while jogo == "sim":
+    print("ok, vamos lá")
+    soma = int(input("quanto é 1 + 3?"))
+    if soma == 4:
+        print("parabéns, voce acertou!")
+        jogo = str(input("quer jogar novamente?"))
+    else: 
+        print("resposta errada")
+        jogo = str(input("quer tentar novamente?"))
+print("tudo bem {}. até a proxima!".format(nome))
