549df7ab62c88fd99e030a01642068b1bfa95aed
Pedro Henrique Freitas da Silva <151584153+1freitasp@users.noreply.github.com>
2025-02-18 23:11:57 -0300

    Update z1.py

--- z1.py
+++ z1.py
@@ -14,3 +14,21 @@ while jogo == "sim":
         print("resposta errada")
         jogo = str(input("quer tentar novamente?"))
 print("tudo bem {}. até a proxima!".format(nome))
+
+## atualizaçao do código, dessa vez com mais funcionalidades
+
+nome = str(input('olá! qual o seu nome?'))
+pergunta = str(input(f'olá {nome}, posso te fazer uma pergunta?'))
+while pergunta == 'sim':
+    n1 = int(input('me diga um numero inteiro'))
+    n2 = int(input('diga outro'))
+    soma = int(input('agora me diga a soma desses dois numeros'))
+    if soma == n1 + n2:
+        print(f'a soma entre {n1} e {n2} é {n1 + n2}') 
+        print('parabens, voce acertou!')
+        pergunta = str(input('quer continuar? (sim/nao)'))
+    else:
+        print('voce errou')
+    print(f'a soma entre {n1} e {n2} é {n1 + n2}') 
+    pergunta = str(input('quer tentar novamente?'))
+print(f'tudo bem {nome}, até a proxima!')
